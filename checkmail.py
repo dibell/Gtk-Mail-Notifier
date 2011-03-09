ab210de691a1e5d8788e9e30be97921d1198b706
David Bell <davidb@isotoma.com>
2011-03-09 10:20:02 +0000

    initial checkin

---
+++ checkmail.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+import pygtk
+pygtk.require('2.0')
+import gtk
+import gobject
+import imaplib
+from datetime import datetime
+
+class HelloTray:
+  def __init__(self):
+    self.statusIcon = gtk.StatusIcon()
+    self.statusIcon.set_from_file('gmail-pencil24-grey.png')
+    self.statusIcon.set_visible(True)
+
+    self.menu = gtk.Menu()
+    self.menuItem = gtk.MenuItem('Check mail')
+    self.menuItem.connect('activate', self.my_timer)
+    self.menu.append(self.menuItem)
+    self.menuItem = gtk.ImageMenuItem(gtk.STOCK_QUIT)
+    self.menuItem.connect('activate', self.quit_cb, self.statusIcon)
+    self.menu.append(self.menuItem)
+    self.statusIcon.connect('popup-menu', self.popup_menu_cb, self.menu)
+    self.statusIcon.set_visible(1)
+
+    gobject.timeout_add_seconds(7*60, self.my_timer)
+
+    self.M = imaplib.IMAP4_SSL('imap.gmail.com')
+    config = open('pw')
+    username = config.readline().rstrip()
+    password = config.readline().rstrip()
+    config.close()
+    self.M.login(username, password)
+
+    self.my_timer()
+
+    gtk.main()
+
+  def my_timer(self, *args):
+    self.M.select()
+    typ, data = self.M.search(None, 'UNSEEN')
+    #print typ, data
+    if data[0]: # unseen mail arrived
+      self.statusIcon.set_from_file('gmail-pencil24.png')
+    else:
+      self.statusIcon.set_from_file('gmail-pencil24-grey.png')
+
+    msgnums = data[0].split()
+
+    status = ''
+    for num in msgnums:
+        typ, data = self.M.fetch(num, '(RFC822.HEADER)')
+        headers = data[0][1].split('\r\n')
+        for header in headers:
+            if header.startswith('Subject:') or header.startswith('From:'):
+                status += header + '\n'
+    status += "(%d) Last checked: %s" % (len(msgnums), datetime.now().strftime('%H:%M:%S'))
+    self.statusIcon.set_tooltip(status)
+
+    return True
+
+  #def execute_cb(self, widget, event, data = None):
+    #window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+    #window.set_border_width(10)
+    #button = gtk.Button("Hello World")
+    #button.connect_object("clicked", gtk.Widget.destroy, window)
+    #window.add(button)
+    #button.show()
+    #window.show()
+
+  def quit_cb(self, widget, data = None):
+    self.M.close()
+    self.M.logout()
+    gtk.main_quit()
+
+  def popup_menu_cb(self, widget, button, time, data = None):
+    if button == 3:
+      if data:
+        data.show_all()
+        data.popup(None, None, gtk.status_icon_position_menu,
+                   3, time, self.statusIcon)
+
+if __name__ == "__main__":
+  helloWord = HelloTray()
